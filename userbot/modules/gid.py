873140b20a0833500dd26de589cb33615cbb86f6
maspion27 <71593098+maspion27@users.noreply.github.com>
2021-07-05 12:38:09 +0700

    Update gid.py

--- userbot/modules/gid.py
+++ userbot/modules/gid.py
@@ -39,7 +39,7 @@ async def image_maker(event):
 
 CMD_HELP.update(
     {
-        "gid": "⚡**CMD**⚡ : `.gid`\
+        "gid": " 🐣**CMD**🐣  : `.gid`\
         \n↳ : Reply to a user to generate ID Card."
     }
 )
