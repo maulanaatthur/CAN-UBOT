9d4aabad4e37ecda5abc3f728e38ea63fc5e5ce2
maulanaatthur <83605962+maulanaatthur@users.noreply.github.com>
2021-07-08 14:08:24 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -82,11 +82,11 @@ async def gben(userbot):
     sender = await dc.get_sender()
     me = await dc.client.get_me()
     if not sender.id == me.id:
-        dark = await dc.reply("`Ingin Mengaktifkan Perintah Global Banned!`")
+        dark = await dc.reply("`Ingin Menghilangkan Dia Dari Dunia!`")
     else:
-        dark = await dc.edit("`Memproses Global Banned Pengguna Ini!!`")
+        dark = await dc.edit("`Memanggil Izrail!!`")
     me = await userbot.client.get_me()
-    await dark.edit(f"`Global Banned Akan Segera Aktif!!!`")
+    await dark.edit(f"`Menertawakan Jasadnya!!!`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
     f"@{me.username}" if me.username else my_mention
     await userbot.get_chat()
@@ -127,7 +127,7 @@ async def gben(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, view_messages=False)
                 a += 1
-                await dark.edit(f"`Global Banned Aktif ✅`")
+                await dark.edit(f"`Dia Sudah Tewas ✅`")
             except BaseException:
                 b += 1
     else:
